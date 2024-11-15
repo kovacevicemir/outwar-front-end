d0d7be44f244f8ce2e9306adea765b75568e93a1
Emir Kovacevic <Emir.Kovacevic@lynkz.com.au>
2024-11-15 22:56:41 +1000

    adding equiped items logic to fetch data from backend and normalize it - displaying equiped items

--- src/app/components/equiped-items/equiped-items.component.ts
+++ src/app/components/equiped-items/equiped-items.component.ts
@@ -1,4 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+type Item = {
+  id: number;
+  name: string;
+  stats: number[]; // Array of numbers for stats
+  setBonus: number[]; // Array of numbers for set bonuses
+  upgradeLevel: number;
+  type: string;
+  attack?: number;
+  hp?: number;
+  maxRage?: number;
+  rage?: number;
+  exp?: number;
+  rampage?: number;
+  critical?: number;
+  block?: number;
+  urlPath?: string
+};
+
+type EquipedItems = {
+  head?: Item;
+  neck?: Item;
+  weapon?: Item;
+  armor?: Item;
+  shield?: Item;
+  belt?: Item;
+  shoes?: Item;
+  ring?: Item;
+};
+
+type UserResponse = {
+  id: number;
+  name: string;
+  level: number;
+  experience: number;
+  items: {
+      id: number;
+      name: string;
+      stats: number[];
+      setBonus: number[];
+      upgradeLevel: number;
+      type: string;
+  }[];
+  equipedItemsId: number[];
+  questMonsterIds: number[];
+}
 
 @Component({
   selector: 'app-equiped-items',
@@ -6,71 +53,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equiped-items.component.css'],
 })
 export class EquipedItemsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.getUserByUsername('test1');
   }
+
+  getUserByUsername(username: string) {
+    const url = `https://localhost:44338/get-user-by-username?username=${username}`;
+    this.http.get(url).subscribe({
+      next: (response) => {
+        console.log('User data:', response);
+        this.generateEquipedItems(response as UserResponse)
+      },
+      error: (error) => {
+        console.error('Error fetching user:', error);
+      },
+    });
+  }
+
+  
+
+  generateEquipedItems(user: UserResponse) {
+    //Compare equipedItems to Items and find which items that player owns are actually equiped
+    const findEquipedItems = user.items.filter(item =>
+      user.equipedItemsId.includes(item.id)
+    );
+
+    //   0   | 1 |   2   |  3 | 4 |   5   |    6   |  7  
+    // attack|hp |maxRage|rage|exp|rampage|critical|block
+    // Normalize stats - Translate stats array to real stats - normalize
+    // Attach image url to item as well
+
+    const normalizedEquipedItems: Item[] = [];
+
+    findEquipedItems.forEach(item => {
+      const normalizedItem = {
+        ...item, 
+        attack: item.stats[0],
+        hp: item.stats[1],
+        maxRage: item.stats[2],
+        rage: item.stats[3],
+        exp: item.stats[4],
+        rampage: item.stats[5],
+        critical: item.stats[6],
+        block: item.stats[7],
+        urlPath: this.resolveImagePath(item)
+      }
+
+      normalizedEquipedItems.push(normalizedItem);
+    })
+
+    //Equip normalized items
+    normalizedEquipedItems.forEach(item =>{
+      this.equipedItems[item.type as keyof typeof this.equipedItems] = item;
+    })
+  }
+
+  resolveImagePath(item: Item | undefined){
+    if(item === undefined){
+      return "resolve image path went wrong! - replace with default img 2"
+    }
+
+    if(item.name.includes("Champions")){
+      return `../assets/championset/champ${item.type}.gif`
+    }
+
+    return "resolve image path went wrong! - replace with default img"
+  }
+
 
   title = 'micro-outwar';
   activeItem = '';
-  equipedItems = {
-    head: {
-      name: 'Head-Gear of Champions',
-      hp: 150,
-      rage: 20,
-      exp: 20,
-      rampage: 4,
-    },
-    neck: {
-      name: 'Sweaty Towel of Champions',
-      hp: 150,
-      rage: 35,
-      exp: 10,
-      critical: 5,
-    },
-    weapon: {
-      name: 'Precision Glove of Champions',
-      atk: 85,
-      rage: 32,
-      exp: 15,
-      maxRage: 500,
-      critical: 4,
-    },
-    armor: {
-      name: 'Silken Shroud of Champions',
-      hp: 155,
-      rage: 23,
-      exp: 17,
-      maxRage: 500,
-      critical: 5,
-    },
-    shield: {
-      name: 'Power Glove of Champions',
-      hp: 150,
-      rage: 15,
-      exp: 23,
-      maxRage: 400,
-      block: 7,
-      critical: 4,
-    },
-    belt: {
-      name: 'Title Belt of Champions',
-      hp: 150,
-      exp: 25,
-      rampage: 5,
-      critical: 5,
-    },
-    shoes: {
-      name: 'Sneakers of Champions',
-      hp: 150,
-      rage: 15,
-      exp: 25,
-      rampage: 4,
-      critical: 5,
-    },
-    ring: { name: 'Medical Tape of Champions', rage: 22, exp: 22, critical: 4 },
-  };
+  equipedItems: EquipedItems = {};
 
   setActiveItem(item: string) {
     if (this.activeItem === item) {
@@ -79,5 +134,4 @@ export class EquipedItemsComponent implements OnInit {
       this.activeItem = item;
     }
   }
-
 }
--- src/app/components/equiped-items/equiped-items.module.ts
+++ src/app/components/equiped-items/equiped-items.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EquipedItemsComponent } from './equiped-items.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, HttpClientModule
   ],
   declarations: [EquipedItemsComponent],
   exports: [EquipedItemsComponent]
